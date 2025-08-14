940649a5bd42c3d16c68352e944f3a9c728aeb63
Aubain-web <69096601+Aubain-web@users.noreply.github.com>
2025-08-14 22:38:28 +0200

    setup suprss frontend

---
+++ suprss-frontend/src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="min-h-screen grid place-items-center">
+        <div className="p-6 rounded-2xl shadow border max-w-md w-full">
+          <h1 className="text-2xl font-semibold mb-2">SUPRSS </h1>
+          <p className="opacity-80">setup</p>
+          <button className="mt-4 px-4 py-2 rounded bg-black text-white dark:bg-white dark:text-black">
+            Bouton test
+          </button>
+        </div>
+      </div>
+
+    </>
+  )
+}
+
+export default App
